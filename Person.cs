c7d913b9391bc1b08e4b2fd6d5b61e014aeb43e6
ardhel08 <ardeliacraft@gmail.com>
2023-06-02 21:07:20 +0700

    inheritance

---
+++ Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace inheritance
+{
+    //class Person atau class utama
+    public class Person
+    {
+        //field dari class Person
+        string name;
+        int age;
+    
+        //constructor dari class Person
+        public Person(string n, int a) 
+        {
+            this.name = n;
+            this.age = a;
+        }
+        
+
+        //fungsi/method yang akan menampilkan nama dan umur
+        public void InfoPerson()
+        {
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", name, age);
+        }
+    }
+
+        //kelas karyawan sebagai kelas turunan
+        public class Karyawan : Person 
+    {
+
+        //properties dari kelas karyawan
+        public string karyawanid;
+        public string subject; 
+
+        //constructor dari kelas karyawan
+        public Karyawan( string kid, string sbj, string n, int a) : base(n, a)
+        {
+            karyawanid = kid;
+            subject = sbj;
+
+        }
+  
+        //method dari kelas karyawan
+        public void InfoKaryawan()
+        {
+            
+            Console.WriteLine("Dengan ID {0} dan saya sebagai {1}", karyawanid, subject);
+        }
+        //override method dari class Person
+        public void InfoPerson(string name, int age)
+        {
+            
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun", name, age);
+        }
+
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("( hasil output dari class Person )");
+            Person person1 = new Person("Brian Kang", 19);
+            person1.InfoPerson();
+            Console.WriteLine("");
+            Console.WriteLine("================================");
+            Console.WriteLine("");
+            Console.WriteLine("( hasil output dari class Karyawan )");
+            Karyawan karyawan1 = new Karyawan("09876545", "Dosen", "Jae", 29);
+            karyawan1.InfoPerson();
+            karyawan1.InfoKaryawan();
+            
+          
+            Console.ReadKey();
+        }
+
+    }
+}
